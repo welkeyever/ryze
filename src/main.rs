42e69d6cfe58099c904f971ddfad238f1d19e20a
gaowenju <gaowenju@bytedance.com>
2023-11-16 14:35:29 +0800

    feat: import trie to handler route

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,57 @@
-extern crate route_recognizer;
 use std::net::SocketAddr;
-use hyper::{Body, Error, Method, Request, Response, Server, StatusCode};
+use std::pin::Pin;
+use std::sync::Arc;
+use futures::future::Future;
+use hyper::{Body, Request, Response, Server, StatusCode};
 use hyper::service::{make_service_fn, service_fn};
-use route_recognizer::{Router, Match};
+use route_recognizer::{Router, Params};
+use tokio::sync::Mutex;
 
-async fn handler(req: Request<Body>, matched: Match<&String>) -> Result<Response<Body>, Error> {
-    match (req.method(), matched.handler.as_str()) {
-        (&Method::GET, "/") => {
-            if let Some(query) = req.uri().query() {
-                return Ok(Response::new(Body::from(query.to_owned())))
-            }
-            let mut default = Response::default();
-            *default.body_mut() = Body::from("ok");
-            Ok(default)
-        }
-        _ => {
-            let mut not_found = Response::default();
-            *not_found.status_mut() = StatusCode::NOT_FOUND;
-            Ok(not_found)
-        }
-    }
+type BoxFut = Pin<Box<dyn Future<Output = Result<Response<Body>, hyper::Error>> + Send>>;
+type Handler = Arc<dyn Fn(Request<Body>) -> BoxFut + Send + Sync>;
+
+fn index(_req: Request<Body>) -> BoxFut {
+    Box::pin(async {
+        let response = Response::new(Body::from("Hello, index!"));
+        Ok(response)
+    })
+}
+
+fn hello(_req: Request<Body>) -> BoxFut {
+    Box::pin(async {
+        let response = Response::new(Body::from("Hello, hello!"));
+        Ok(response)
+    })
 }
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    let addr = SocketAddr::from(([127, 0, 0, 1], 8888));
-    let mut router = Router::new();
-    router.add("/", "/".to_owned());
+async fn main() -> hyper::Result<()> {
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8000));
 
-    let make_svc = make_service_fn(move |_conn| {
-        // Clone router here
-        let router = router.clone();
-        async move {
-            Ok::<_, hyper::Error>(service_fn(move |req| {
-                let path = req.uri().path().to_owned();
-                // Clone router again inside async block
-                let router = router.clone();
-                // Handler needs to be async block instead of a function
+    let mut router: Router<Handler> = Router::new();
+    let router = Arc::new(Mutex::new(router));
+
+    router.lock().await.add("/", Arc::new(index));
+    router.lock().await.add("/hello", Arc::new(hello));
+
+    let make_svc = make_service_fn(move |_| {
+        let router = Arc::clone(&router);
+        async {
+            Ok::<_, hyper::Error>(service_fn(move |req: Request<Body>| {
+                let router = Arc::clone(&router);
                 async move {
-                    let matched = router.recognize(&path).unwrap();
-                    handler(req, matched).await
+                    let path = req.uri().path().to_owned();
+                    let router = router.lock().await;
+                    match router.recognize(&path) {
+                        Ok(matched) => (matched.handler)(req).await,
+                        Err(_) => Ok(Response::builder().status(StatusCode::NOT_FOUND).body(Body::empty()).unwrap()),
+                    }
                 }
             }))
         }
     });
 
     let server = Server::bind(&addr).serve(make_svc);
-
     println!("Listening on http://{}", addr);
 
     server.await?;

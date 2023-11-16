9e1b6f31ba8f48e835dbe33b254f8079b477893a
gaowenju <gaowenju@bytedance.com>
2023-11-16 14:39:03 +0800

    chore: little played by welkeyever

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,26 @@
 use std::net::SocketAddr;
 use std::pin::Pin;
 use std::sync::Arc;
+
 use futures::future::Future;
 use hyper::{Body, Request, Response, Server, StatusCode};
 use hyper::service::{make_service_fn, service_fn};
-use route_recognizer::{Router, Params};
+use route_recognizer::{Params, Router};
 use tokio::sync::Mutex;
 
-type BoxFut = Pin<Box<dyn Future<Output = Result<Response<Body>, hyper::Error>> + Send>>;
+type BoxFut = Pin<Box<dyn Future<Output=Result<Response<Body>, hyper::Error>> + Send>>;
 type Handler = Arc<dyn Fn(Request<Body>) -> BoxFut + Send + Sync>;
 
 fn index(_req: Request<Body>) -> BoxFut {
     Box::pin(async {
-        let response = Response::new(Body::from("Hello, index!"));
+        let response = Response::new(Body::from("Hello, world!"));
         Ok(response)
     })
 }
 
 fn hello(_req: Request<Body>) -> BoxFut {
     Box::pin(async {
-        let response = Response::new(Body::from("Hello, hello!"));
+        let response = Response::new(Body::from("Pong!"));
         Ok(response)
     })
 }
@@ -28,7 +29,7 @@ fn hello(_req: Request<Body>) -> BoxFut {
 async fn main() -> hyper::Result<()> {
     let addr = SocketAddr::from(([127, 0, 0, 1], 8000));
 
-    let mut router: Router<Handler> = Router::new();
+    let router: Router<Handler> = Router::new();
     let router = Arc::new(Mutex::new(router));
 
     router.lock().await.add("/", Arc::new(index));

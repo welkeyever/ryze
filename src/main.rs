01ed5962107448822fab61c3dd37e6fffb147315
gaowenju <gaowenju@bytedance.com>
2023-11-20 15:04:25 +0800

    feat: add RequestContext and almost there

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,28 @@
 use std::net::SocketAddr;
-use std::pin::Pin;
 use std::sync::Arc;
 
-use futures::future::Future;
-use hyper::{Body, Request, Response, Server, StatusCode};
-use hyper::service::{make_service_fn, service_fn};
-use route_recognizer::{Params, Router};
-use tokio::sync::Mutex;
+use hyper::Body;
 
-mod uppercase_middleware;
+use hertz::{Handler, Hertz, RequestContext};
+
 mod hertz;
 
-type BoxFut = Pin<Box<dyn Future<Output=Result<Response<Body>, hyper::Error>> + Send>>;
-type Handler = Arc<dyn Fn(Request<Body>) -> BoxFut + Send + Sync>;
-
-fn index(req: Request<Body>) -> BoxFut {
-    Box::pin(async {
-        let (header, _) = req.into_parts();
-        if let Some(query) = header.uri.query() {
-            let response = Response::new(Body::from(query.chars().map(|b| b.to_ascii_uppercase()).collect::<String>()));
-            return Ok(response);
-        }
-
-        let response = Response::new(Body::from("Hello, world!"));
-        Ok(response)
-    })
+// Index handler
+fn index(ctx: &mut RequestContext) {
+    let query = ctx.req.uri().query().unwrap_or("");
+    let body = query.chars().map(|b| b.to_ascii_uppercase()).collect::<String>();
+    *ctx.resp.body_mut() = Body::from(body);
 }
 
-fn hello(_req: Request<Body>) -> BoxFut {
-    Box::pin(async {
-        let response = Response::new(Body::from("Pong!"));
-        Ok(response)
-    })
+// Hello (or ping) handler
+fn hello(ctx: &mut RequestContext) {
+    *ctx.resp.body_mut() = Body::from("Pong!");
 }
 
 #[tokio::main]
 async fn main() -> hyper::Result<()> {
-    let mut h = hertz::Hertz::new();
+    let mut h = Hertz::new();
+    h.get("/query", Arc::new(index));
     h.get("/ping", Arc::new(hello));
     h.spin(SocketAddr::from(([127, 0, 0, 1], 8000))).await
 }
